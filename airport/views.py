b588f219d5e96a83703e3e436dc06fb57e6fc39e
Artur Poltser <polcerartur@gmail.com>
2024-03-23 15:58:16 +0200

    feat(airport): register endpoint for crew model (serializer+view+url)

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from airport.models import Crew
+from airport.serializers.crew_serializers import CrewSerializer
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
