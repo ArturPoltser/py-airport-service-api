f2c58ab6a97f9860787997850bcef761e5a8948b
Artur Poltser <polcerartur@gmail.com>
2024-03-23 15:58:16 +0200

    feat(airport): register endpoint for crew model (serializer+view+url)

--- airport/urls.py
+++ airport/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
+from rest_framework import routers
+
+from airport.views import CrewViewSet
+router = routers.DefaultRouter()
+router.register("crews", CrewViewSet)
+
+urlpatterns = router.urls
 
 app_name = "airport"
