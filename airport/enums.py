1d4239d81733422af3194600e44fb589d0ae7bff
Artur Poltser <polcerartur@gmail.com>
2024-03-23 15:19:51 +0200

    feat(airport): implement airport models

---
+++ airport/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class CrewPositionEnum(Enum):
+    PILOT = "pilot"
+    COPILOT = "copilot"
+    FLIGHT_ATTENDANT = "flight_attendant"
+    AIR_TRAFFIC_CONTROLLER = "air_traffic_controller"
+    GROUND_CREW = "ground_crew"
+    SECURITY_OFFICER = "security_officer"
+    AIRPORT_STAFF = "airport_staff"
