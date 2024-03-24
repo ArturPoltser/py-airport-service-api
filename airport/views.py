2d65e83fc0e3fa31cee80d538611c0f95fb68adb
Artur Poltser <polcerartur@gmail.com>
2024-03-24 18:50:39 +0200

    feat(airport): add advanced view & filtration feature for FlightViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import F, Count
 from rest_framework import mixins, viewsets
 
 from airport.models import (
@@ -15,7 +18,11 @@ from airport.serializers.airplane_type_serializers import (
 )
 from airport.serializers.airport_serializers import AirportSerializer
 from airport.serializers.crew_serializers import CrewSerializer
-from airport.serializers.flight_serializers import FlightSerializer
+from airport.serializers.flight_serializers import (
+    FlightSerializer,
+    FlightListSerializer,
+    FlightDetailSerializer
+)
 from airport.serializers.order_serializers import (
     OrderSerializer,
 )
@@ -76,13 +83,14 @@ class RouteViewSet(
     def get_queryset(self):
         queryset = self.queryset
 
-        if source := self.request.query_params.get("source"):
-            queryset = queryset.filter(source__name__icontains=source)
+        if self.action == "list":
+            if source := self.request.query_params.get("source"):
+                queryset = queryset.filter(source__name__icontains=source)
 
-        if destination := self.request.query_params.get("destination"):
-            queryset = queryset.filter(
-                destination__name__icontains=destination
-            )
+            if destination := self.request.query_params.get("destination"):
+                queryset = queryset.filter(
+                    destination__name__icontains=destination
+                )
 
         return queryset.distinct()
 
@@ -99,6 +107,70 @@ class RouteViewSet(
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.action == "list":
+            current_time = datetime.now()
+
+            queryset = (
+                queryset
+                .select_related(
+                    "route__source",
+                    "route__destination",
+                    "airplane"
+                )
+                .filter(departure_time__gt=current_time)
+                .annotate(
+                    tickets_available=(
+                        F("airplane__rows") * F("airplane__seats_in_row")
+                        - Count("tickets")
+                    )
+                )
+            )
+
+            if from_city := self.request.query_params.get("from"):
+                queryset = queryset.filter(
+                    route__source__closest_big_city__icontains=from_city
+                )
+
+            if to_city := self.request.query_params.get("to"):
+                queryset = queryset.filter(
+                    route__destination__closest_big_city__icontains=to_city
+                )
+
+            if departure := self.request.query_params.get("departure_date"):
+                departure_date = datetime.strptime(departure, "%Y-%m-%d")
+                queryset = queryset.filter(
+                    departure_time__date=departure_date
+                )
+
+            if arrival := self.request.query_params.get("arrival_date"):
+                arrival_date = datetime.strptime(arrival, "%Y-%m-%d")
+                queryset = queryset.filter(arrival_time__date=arrival_date)
+
+        if self.action == "retrieve":
+            queryset = (
+                queryset
+                .select_related(
+                    "airplane__airplane_type",
+                    "route__source",
+                    "route__destination"
+                )
+                .prefetch_related("crew")
+            )
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+
+        if self.action == "retrieve":
+            return FlightDetailSerializer
+
+        return FlightSerializer
 
 
 class OrderViewSet(
