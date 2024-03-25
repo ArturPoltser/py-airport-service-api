1cbe5bf71f06b992d480b4e402e2f01f72448705
Artur Poltser <polcerartur@gmail.com>
2024-03-25 09:26:43 +0200

    feat(config): add default throttles to the project

--- config/settings.py
+++ config/settings.py
@@ -142,5 +142,10 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5
+    "PAGE_SIZE": 5,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "30/day", "user": "150/day"},
 }
