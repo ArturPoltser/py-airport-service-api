1d7937dd8a01305cab7adf5a115c1ec978bbb20e
Artur Poltser <polcerartur@gmail.com>
2024-03-28 10:43:37 +0200

    feat(airport): fix N + 1 problem & add feature for creating orders with tickets in admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -11,11 +11,45 @@ from airport.models import (
     Ticket
 )
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
+@admin.register(Airplane)
+class AirplaneAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(AirplaneAdmin, self).get_queryset(request)
+        return queryset.select_related("airplane_type")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(RouteAdmin, self).get_queryset(request)
+        return queryset.select_related("source", "destination")
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(FlightAdmin, self).get_queryset(request)
+        return queryset.select_related(
+            "route__source",
+            "route__destination",
+            "airplane"
+        )
+
+
 admin.site.register(Crew)
 admin.site.register(AirplaneType)
-admin.site.register(Airplane)
 admin.site.register(Airport)
-admin.site.register(Route)
-admin.site.register(Flight)
-admin.site.register(Order)
-admin.site.register(Ticket)
